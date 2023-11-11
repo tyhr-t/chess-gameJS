9d5ecb2aacff76c2e289142084387e22b1567bec
tidiane <kreyze@macbook-air-de-tidiane.home>
2023-11-11 23:17:34 +0100

    codinglb version

--- src/components/cheesBoardGame.jsx
+++ src/components/cheesBoardGame.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from "react"
 import { generateBoard } from "../utils/generateBoard"
-import { defaultConfigChessBoard } from "../utils/defaultConfigChessBoard"
 
 export const ChessBoard = () => {
-  const [config] = useState(defaultConfigChessBoard)
-  const chessBoard = generateBoard()
+  const [chessBoard /* setChessBoard */] = useState(generateBoard())
+  /**
+   State pour déterminer quel pion est sélectionné : [selectedPawnState, setSelectedPawnState] = useState(null)
+   chessBoard[selectedPawnState.y][selectedPawnState.x] => La valeur présente dans une case.
+   Pour sélectionner le pion, l'utilisateur doit cliquer sur la case où se trouve le pion.
+   On sauvegarde la position du pion dans le state.
+   Pour faire bouger le pion, l'utilisateur doit cliquer sur une case vide => ça modifie chessBoard en fonction de selectedPawnState.
+   Une fois que le pion est bougé, le state doit repasser sur null.
+   */
+  const clickedOnSquare = (x, y) => console.log(x, y)
 
   return (
     <div>
-      {chessBoard.map((line, lineIndex) => (
-        <div className="flex border" key={lineIndex}>
-          {line.map((square, elementOfLineIndex) => {
-            const lineIsPair = lineIndex % 2 === 0
+      {chessBoard.map((line, y) => (
+        <div className="flex border" key={y}>
+          {line.map((square, x) => {
+            const lineIsPair = y % 2 === 0
             const offset = lineIsPair ? 0 : 1
-            const isPair = (elementOfLineIndex + offset) % 2 === 0
+            const isBlack = (x + offset) % 2 === 0
 
             return (
               <div
-                className={`w-20 h-20 bg-${isPair ? "" : ""}`}
-                style={{ backgroundColor: isPair ? "#616198" : "" }}
-                key={`${square}-${lineIndex}`}>
-                {config[square]}
+                onClick={() => clickedOnSquare(x, y)}
+                className={`w-20 h-20 ${isBlack ? "bg-blue-300" : ""}`}
+                key={`${x}-${y}`}>
+                {`${chessBoard[y][x]} | (${x} - ${y})`}
               </div>
             )
           })}
--- src/utils/generateBoard.js
+++ src/utils/generateBoard.js
@@ -1,8 +1,11 @@
 export const generateBoard = () => {
   const lines = Array.from(Array(8).keys())
   const squares = Array.from(Array(8).keys())
+  // return lines.map((line) =>
+  //   squares.map((squareNumber) => Number(`${line}${squareNumber}`))
+  // )
+  const board = lines.map(() => squares.map(() => Number(`0`)))
+  board[0][0] = "1"
 
-  return lines.map((line) =>
-    squares.map((squareNumber) => Number(`${line}${squareNumber}`))
-  )
+  return board
 }
